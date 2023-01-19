942c112b8724c19e5f2a0537a83c6c7cf76b9dbc
MaxDefense <49471411+MaxDefense@users.noreply.github.com>
2023-01-19 09:22:28 +0300

    add 🧙‍♂️ start-vscoding.py

---
+++ start-vscoding.py
@@ -0,0 +1,152 @@
+
+##########################*
+
+# comment dedicated
+#! Ctrl + /
+
+# format document
+#! Shit + Alt + F
+# format selection
+#! Ctrl + K --> Ctrl + F
+
+#? C:\Users\YourName\AppData\Roaming\Code\User\settings.json
+#? C:\VSCode.Git.Bundle\VSCode\data\user-data\User\settings.json
+
+##########################*
+#TODO                                GIT
+
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+
+##########################*
+
+#! директория = каталог = папка
+
+#? print working directory | вывести путь к рабочей директории
+# $ pwd
+#? list files and directories | вывести содержимое заданной директории
+# $ ls
+#? change directory | сменить рабочую директорию
+# $ cd
+#? make directory | создать новую директорию
+# $ mkdir
+#? remove empty directory | удалить пустую директорию
+# $ rmdir
+#? remove files or directories | удалить файлы или директории
+# $ rm
+#? copy files or directories | копировать файлы или директории
+# $ cp
+#? move and/or rename files | переместить и/или переименовать файлы
+# $ mv
+#? disk free | вывести информацию об объеме свободного пространства в каждом из смонтированных разделов
+# $ df
+
+##########################*
+#TODO                                PYTHON
+
+'''
+#?  Windows / bash
+$ python -m venv env
+$ source env/Scripts/activate
+$ deactivate
+#?  Windows \ cmd
+> python -m venv env
+> env\Scripts\activate.bat
+> env\Scripts\deactivate.bat
+#?  Linux
+$ python3.8 -m venv env
+$ source env/bin/activate
+$ deactivate
+'''
+
+##########################*
+
+# $ python -V
+# $ python --version
+# Python 3.8.10
+
+# $ pip -V
+# $ pip --version
+# pip 21.1.1
+
+# $ python -m pip install --upgrade pip
+
+# $ pip freeze
+# $ pip list
+
+# $ pip freeze > requirements.txt
+
+##########################*
+
+# $ pip install pyinstaller
+
+# $ pyinstaller --onefile -n=RockScissorsPaper --icon=img/hand.ico code/rock_scissors_paper.py
+
+##########################*
+
+# $ pip install pygame
+
+##########################*
+
+# $ pip install PyQt5
+
+##########################*
+
+# $ pip install Flask
+
+##########################*
+
+# $ pip install Django
+
+# $ django-admin startproject hello_django
+# $ cd hello_django
+# $ python manage.py runserver 0.0.0.0:5000
+
+##########################*
+
+# $ pip install Jinja2
+
+##########################*
+#TODO                                JS
+
+# $ node -v
+# $ node --version
+# v13.14.0
+# v16.18.0
+
+# $ npm -v
+# $ npm --version
+# 8.19.2
+
+# npm install -g npm
+
+##########################*
+#TODO                                GO
+
+# $ go version
+# go version go1.19.3 windows/amd64
+
+##########################*
+
+# $ go mod init example.com/project
+# $ go run main.go
+# $ go build main.go
+
+##########################*
+#TODO                                C
+
+# $ gcc --version
+#? C:\My_Program\MinGW\bin;
+# gcc.exe (MinGW.org GCC Build-2) 9.2.0
+#? C:\My_Program\cygwin64\bin;
+# gcc (GCC) 11.3.0
+
+##########################*
+
+# $ gcc main.c -o main.exe
+# $ ./main
+# $ gcc main.c -o main.exe && ./main
+
+# $ gcc main.c -o main.exe -lcurses
+
+##########################*
